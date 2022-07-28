b1b05c0bcd83856e95dac999287486fef5e3886e
Juan <casal.juanmanuel@gmail.com>
2022-07-28 10:36:26 -0300

    agregando requeriments

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
+
 class HomePageView(TemplateView):
     template_name = "core/index.html"
+
+    def current_task(loop=None):
+        """Return a currently executed task."""
+        if loop is None:
+            loop = events.get_running_loop()
+        return _current_tasks.get(loop)
